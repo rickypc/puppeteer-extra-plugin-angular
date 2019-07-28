8e701f0eeb4e9559098ae01c3eacf84b32110bde
Richard Huang <rickypc@users.noreply.github.com>
2019-07-28 00:49:41 -0700

    Adjusts the line break.

--- __tests__/Form.test.js
+++ __tests__/Form.test.js
@@ -42,7 +42,8 @@ describe('Form module test', () => {
     });
 
     it('should return rejected', async () => {
-      await Form.fillOut().catch(ex => expect(ex.message).toEqual('Form config should be an array.'));
+      await Form.fillOut().catch(ex => expect(ex.message)
+        .toEqual('Form config should be an array.'));
       expect(mock.fillOutField).not.toHaveBeenCalled();
       expect(mock.getValue).not.toHaveBeenCalled();
     });
