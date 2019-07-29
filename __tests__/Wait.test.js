3e2bfd8a4e466c90ed09e291b3674bce8cbb8c76
Richard Huang <rickypc@users.noreply.github.com>
2019-07-28 17:30:25 -0700

    Adds Wait module tests.

---
+++ __tests__/Wait.test.js
@@ -0,0 +1,281 @@
+/*!
+ *  Wait.test.js - tests for Wait functionality.
+ *  Copyright (c) 2018 - 2019 Richard Huang <rickypc@users.noreply.github.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Wait = require('../lib/Wait.js');
+
+const mock = {
+  debug: jest.spyOn(Wait.__test__.logger, 'debug'),
+  evaluate: jest.fn((callback, timeout) => new Promise(async (resolve, reject) => {
+    if (this.evaluateAction === 'error') {
+      reject(Error('error'));
+    } else {
+      resolve(await callback(timeout > 100 ? 100 : timeout));
+    }
+  })),
+  waitFor: jest.fn((input, options = {}) => new Promise((resolve, reject) => {
+    if (this.waitForAction === 'error') {
+      reject(Error('error'));
+    } else if (typeof (input) === 'function') {
+      setTimeout(() => resolve(input()), options.timeout > 100 ? 100 : options.timeout);
+    }
+  })),
+};
+
+describe('Wait module test', () => {
+  describe('untilActionReady', () => {
+    beforeAll(() => {
+      this.waitUntilAngularReady = jest.spyOn(Wait.__test__, 'untilAngularReady')
+        .mockImplementation(() => {});
+      this.waitUntilPageReady = jest.spyOn(Wait.__test__, 'untilPageReady')
+        .mockImplementation(() => {});
+    });
+
+    afterAll(() => {
+      this.waitUntilAngularReady.mockRestore();
+      this.waitUntilAngularReady = null;
+      this.waitUntilPageReady.mockRestore();
+      this.waitUntilPageReady = null;
+    });
+
+    it('should return resolved', async () => {
+      await Wait.untilActionReady(100);
+      expect(this.waitUntilPageReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilPageReady).toHaveBeenNthCalledWith(1, Wait, 40);
+      expect(this.waitUntilAngularReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilAngularReady).toHaveBeenNthCalledWith(1, Wait, 100);
+      expect(mock.debug).toHaveBeenCalledTimes(2);
+      expect(mock.debug).toHaveBeenNthCalledWith(1, 'page is ready');
+      expect(mock.debug).toHaveBeenNthCalledWith(2, 'angular is ready');
+    });
+
+    it('should use default value and return resolved', async () => {
+      await Wait.untilActionReady();
+      expect(this.waitUntilPageReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilPageReady).toHaveBeenNthCalledWith(1, Wait, 10000);
+      expect(this.waitUntilAngularReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilAngularReady).toHaveBeenNthCalledWith(1, Wait, 25000);
+      expect(mock.debug).toHaveBeenCalledTimes(2);
+      expect(mock.debug).toHaveBeenNthCalledWith(1, 'page is ready');
+      expect(mock.debug).toHaveBeenNthCalledWith(2, 'angular is ready');
+    });
+  });
+
+  describe('untilAngularReady', () => {
+    beforeAll(() => {
+      this.waitUntilAngularReady = jest.spyOn(Wait.__test__, 'untilAngularReady')
+        .mockImplementation(() => {});
+    });
+
+    afterAll(() => {
+      this.waitUntilAngularReady.mockRestore();
+      this.waitUntilAngularReady = null;
+    });
+
+    it('should return resolved', async () => {
+      await Wait.untilAngularReady(100);
+      expect(this.waitUntilAngularReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilAngularReady).toHaveBeenNthCalledWith(1, Wait, 100);
+      expect(mock.debug).toHaveBeenCalledTimes(1);
+      expect(mock.debug).toHaveBeenNthCalledWith(1, 'angular is ready');
+    });
+
+    it('should use default value and return resolved', async () => {
+      await Wait.untilAngularReady();
+      expect(this.waitUntilAngularReady).toHaveBeenCalledTimes(1);
+      expect(this.waitUntilAngularReady).toHaveBeenNthCalledWith(1, Wait, 25000);
+      expect(mock.debug).toHaveBeenCalledTimes(1);
+      expect(mock.debug).toHaveBeenNthCalledWith(1, 'angular is ready');
+    });
+  });
+
+  describe('Helpers.untilAngularReady', () => {
+    beforeAll(() => {
+      this.context = [];
+      this.context.notifyWhenNoOutstandingRequests = jest.fn((callback) => {
+        setTimeout(callback, this.evaluateTimeout);
+        return this.context;
+      });
+      this.evaluateTimeout = 10;
+      this.injector = jest.fn(() => this.context);
+      global.document = {
+        querySelector: jest.fn(() => {}),
+      };
+    });
+
+    afterAll(() => {
+      global.angular = null;
+      global.document = null;
+      this.context = null;
+      this.injector = null;
+    });
+
+    it('should return truthy', async () => {
+      this.evaluateAction = '';
+      this.context.get = jest.fn(() => this.context);
+      this.context.injector = jest.fn(() => this.context);
+      global.angular = {
+        element: jest.fn(() => this.context),
+        injector: jest.fn(() => this.context),
+      };
+      const actual = await Wait.__test__.untilAngularReady(mock, 100);
+      expect(actual).toBeTruthy();
+      expect(mock.evaluate).toHaveBeenCalledTimes(1);
+      expect(mock.evaluate).toHaveBeenNthCalledWith(1, expect.any(Function), 100);
+      expect(document.querySelector).toHaveBeenCalledTimes(1);
+      expect(document.querySelector).toHaveBeenNthCalledWith(1, '[data-ng-app],[ng-app],.ng-scope');
+      expect(angular.element).toHaveBeenCalledTimes(1);
+      expect(angular.element).toHaveBeenNthCalledWith(1, document);
+      expect(this.context.injector).toHaveBeenCalledTimes(1);
+      expect(this.context.get).toHaveBeenCalledTimes(1);
+      expect(this.context.get).toHaveBeenNthCalledWith(1, '$browser');
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenCalledTimes(1);
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenNthCalledWith(1,
+        expect.any(Function));
+    });
+
+    it('should return truthy on non-angular page', async () => {
+      this.evaluateAction = '';
+      global.angular = null;
+      const actual = await Wait.__test__.untilAngularReady(mock, 100);
+      expect(actual).toBeTruthy();
+      expect(mock.evaluate).toHaveBeenCalledTimes(1);
+      expect(mock.evaluate).toHaveBeenNthCalledWith(1, expect.any(Function), 100);
+      expect(document.querySelector).not.toHaveBeenCalled();
+      expect(this.context.injector).not.toHaveBeenCalled();
+      expect(this.context.get).not.toHaveBeenCalled();
+      expect(this.context.notifyWhenNoOutstandingRequests).not.toHaveBeenCalled();
+    });
+
+    it('should use default value and return truthy', async () => {
+      this.evaluateAction = '';
+      this.context.get = null;
+      this.context.injector = jest.fn(() => {});
+      global.angular = {
+        element: jest.fn(() => this.context),
+        injector: jest.fn(() => this.injector),
+      };
+      const actual = await Wait.__test__.untilAngularReady(mock);
+      expect(actual).toBeTruthy();
+      expect(mock.evaluate).toHaveBeenCalledTimes(1);
+      expect(mock.evaluate).toHaveBeenNthCalledWith(1, expect.any(Function), 25000);
+      expect(document.querySelector).toHaveBeenCalledTimes(1);
+      expect(document.querySelector).toHaveBeenNthCalledWith(1, '[data-ng-app],[ng-app],.ng-scope');
+      expect(angular.element).toHaveBeenCalledTimes(1);
+      expect(angular.element).toHaveBeenNthCalledWith(1, document);
+      expect(this.context.injector).toHaveBeenCalledTimes(1);
+      expect(this.injector).toHaveBeenCalledTimes(1);
+      expect(this.injector).toHaveBeenNthCalledWith(1, '$browser');
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenCalledTimes(1);
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenNthCalledWith(1,
+        expect.any(Function));
+    });
+
+    it('should return falsy', async () => {
+      this.evaluateAction = '';
+      this.context.get = jest.fn(() => this.context);
+      this.context.injector = jest.fn(() => this.context);
+      global.angular = {
+        element: jest.fn(() => this.context),
+        injector: jest.fn(() => this.context),
+      };
+      const actual = await Wait.__test__.untilAngularReady(mock, 5);
+      expect(actual).toBeFalsy();
+      expect(mock.evaluate).toHaveBeenCalledTimes(1);
+      expect(mock.evaluate).toHaveBeenNthCalledWith(1, expect.any(Function), 5);
+      expect(document.querySelector).toHaveBeenCalledTimes(1);
+      expect(document.querySelector).toHaveBeenNthCalledWith(1, '[data-ng-app],[ng-app],.ng-scope');
+      expect(angular.element).toHaveBeenCalledTimes(1);
+      expect(angular.element).toHaveBeenNthCalledWith(1, document);
+      expect(this.context.injector).toHaveBeenCalledTimes(1);
+      expect(this.context.get).toHaveBeenCalledTimes(1);
+      expect(this.context.get).toHaveBeenNthCalledWith(1, '$browser');
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenCalledTimes(1);
+      expect(this.context.notifyWhenNoOutstandingRequests).toHaveBeenNthCalledWith(1,
+        expect.any(Function));
+    });
+
+    it('should log error', async () => {
+      this.evaluateAction = 'error';
+      this.context.get = jest.fn(() => {});
+      this.context.injector = jest.fn(() => {});
+      global.angular = {
+        element: jest.fn(() => this.context),
+        injector: jest.fn(() => this.context),
+      };
+      const actual = await Wait.__test__.untilAngularReady(mock, 5);
+      expect(actual).toBeFalsy();
+      expect(mock.evaluate).toHaveBeenCalledTimes(1);
+      expect(mock.evaluate).toHaveBeenNthCalledWith(1, expect.any(Function), 5);
+      expect(document.querySelector).not.toHaveBeenCalled();
+      expect(angular.element).not.toHaveBeenCalled();
+      expect(this.context.injector).not.toHaveBeenCalled();
+      expect(this.context.get).not.toHaveBeenCalled();
+      expect(this.context.notifyWhenNoOutstandingRequests).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Helpers.untilPageReady', () => {
+    beforeAll(() => {
+      global.document = {
+        readyState: 'complete',
+      };
+    });
+
+    afterAll(() => {
+      global.document = null;
+    });
+
+    it('should return truthy', async () => {
+      this.waitForAction = '';
+      global.document.readyState = 'complete';
+      const actual = await Wait.__test__.untilPageReady(mock, 100);
+      expect(actual).toBeTruthy();
+      expect(mock.waitFor).toHaveBeenCalledTimes(1);
+      expect(mock.waitFor).toHaveBeenNthCalledWith(1, expect.any(Function), { timeout: 100 });
+    });
+
+    it('should use default value and return truthy', async () => {
+      this.waitForAction = '';
+      global.document.readyState = 'complete';
+      const actual = await Wait.__test__.untilPageReady(mock);
+      expect(actual).toBeTruthy();
+      expect(mock.waitFor).toHaveBeenCalledTimes(1);
+      expect(mock.waitFor).toHaveBeenNthCalledWith(1, expect.any(Function), { timeout: 10000 });
+    });
+
+    it('should return falsy', async () => {
+      this.waitForAction = '';
+      global.document.readyState = '';
+      const actual = await Wait.__test__.untilPageReady(mock);
+      expect(actual).toBeFalsy();
+      expect(mock.waitFor).toHaveBeenCalledTimes(1);
+      expect(mock.waitFor).toHaveBeenNthCalledWith(1, expect.any(Function), { timeout: 10000 });
+    });
+
+    it('should log error', async () => {
+      this.waitForAction = 'error';
+      global.document.readyState = 'complete';
+      const actual = await Wait.__test__.untilPageReady(mock);
+      expect(actual).toBeFalsy();
+      expect(mock.waitFor).toHaveBeenCalledTimes(1);
+      expect(mock.waitFor).toHaveBeenNthCalledWith(1, expect.any(Function), { timeout: 10000 });
+      expect(mock.debug).toHaveBeenCalledTimes(1);
+      expect(mock.debug).toHaveBeenNthCalledWith(1, 'untilPageReady error: %s', expect.any(Error));
+    });
+  });
+});
