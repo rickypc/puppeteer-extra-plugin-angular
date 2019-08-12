58ec6cea1a10a73e29bceb390b4f9c58436ce888
Richard Huang <rickypc@users.noreply.github.com>
2019-08-11 18:17:34 -0700

    Adjusts Type module tests.

--- __tests__/Type.test.js
+++ __tests__/Type.test.js
@@ -18,135 +18,141 @@
 
 const Type = require('../lib/Type.js');
 
-const mock = {
-  $eval: jest.fn((selector, callback, value) => new Promise(async (resolve, reject) => {
-    if (this.action === 'error') {
-      reject(Error('error'));
-    } else {
-      resolve(callback(mock.element, value));
-    }
-  })),
-  debug: jest.spyOn(Type.__test__.logger, 'debug'),
-  element: {},
+const createMocks = ({
+  evalAction = '',
+  type = 'text',
+} = {}) => {
+  const mocks = Object.assign({
+    evalAction,
+    type,
+  }, {
+    $eval: jest.fn((selector, callback, value) => new Promise(async (resolve, reject) => {
+      if (mocks.evalAction === 'error') {
+        reject(Error('error'));
+      } else {
+        resolve(callback(mocks.element, value));
+      }
+    })),
+    debug: jest.spyOn(Type.__test__.logger, 'debug'),
+    element: {
+      dispatchEvent: jest.fn(() => {}),
+      type,
+    },
+  });
+  return mocks;
 };
+
+const evtInit = {
+  bubbles: true,
+  cancelable: true,
+};
+
+global.Event = jest.fn(() => {});
 
 describe('Type module test', () => {
   describe('ifExists', () => {
-    beforeAll(() => {
-      this.action = '';
-      this.context = [];
-      this.context.trigger = jest.fn(() => this.context);
-      this.context.val = jest.fn(() => this.context);
-      global.angular = {
-        element: jest.fn(() => this.context),
-      };
-    });
-
-    afterAll(() => {
-      global.angular = null;
-      this.context = null;
-    });
-
     it('should return truthy', async () => {
-      this.action = '';
-      this.context.length = 1;
-      mock.element.type = 'text';
-      const actual = await Type.ifExists.call(mock, 'selector', 'value', 'label');
+      const mocks = createMocks();
+      const actual = await Type.ifExists.call(mocks, 'selector', 'value', 'label');
       expect(actual).toBeTruthy();
-      expect(mock.$eval).toHaveBeenCalledTimes(2);
-      expect(mock.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
-      expect(mock.$eval).toHaveBeenNthCalledWith(2, 'selector', expect.any(Function));
-      expect(mock.debug).toHaveBeenCalledTimes(1);
-      expect(mock.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s', 'value',
+      expect(mocks.$eval).toHaveBeenCalledTimes(1);
+      expect(mocks.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
+      expect(mocks.debug).toHaveBeenCalledTimes(1);
+      expect(mocks.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s', 'value',
         'selector', 'label');
-      expect(angular.element).toHaveBeenCalledTimes(1);
-      expect(angular.element).toHaveBeenNthCalledWith(1, mock.element);
-      expect(this.context.val).toHaveBeenCalledTimes(1);
-      expect(this.context.val).toHaveBeenNthCalledWith(1, 'value');
-      expect(this.context.trigger).toHaveBeenCalledTimes(3);
-      expect(this.context.trigger).toHaveBeenNthCalledWith(1, 'input');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(2, 'change');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(3, 'focusout');
+      expect(mocks.element.value).toEqual('value');
+      expect(mocks.element.dispatchEvent).toHaveBeenCalledTimes(8);
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(1, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(2, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(3, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(4, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(5, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(6, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(7, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(8, expect.any(global.Event));
+      expect(global.Event).toHaveBeenCalledTimes(8);
+      expect(global.Event).toHaveBeenNthCalledWith(1, 'focus', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(2, 'reset', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(3, 'keydown', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(4, 'keypress', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(5, 'keyup', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(6, 'input', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(7, 'change', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(8, 'blur', evtInit);
     });
 
     it('should masked password value and return truthy', async () => {
-      this.action = '';
-      this.context.length = 1;
-      mock.element.type = 'password';
-      const actual = await Type.ifExists.call(mock, 'selector', 'value', 'label');
+      const mocks = createMocks({ type: 'password' });
+      const actual = await Type.ifExists.call(mocks, 'selector', 'value', 'label');
       expect(actual).toBeTruthy();
-      expect(mock.$eval).toHaveBeenCalledTimes(2);
-      expect(mock.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
-      expect(mock.$eval).toHaveBeenNthCalledWith(2, 'selector', expect.any(Function));
-      expect(mock.debug).toHaveBeenCalledTimes(1);
-      expect(mock.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s',
+      expect(mocks.$eval).toHaveBeenCalledTimes(1);
+      expect(mocks.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
+      expect(mocks.debug).toHaveBeenCalledTimes(1);
+      expect(mocks.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s',
         '*****', 'selector', 'label');
-      expect(angular.element).toHaveBeenCalledTimes(1);
-      expect(angular.element).toHaveBeenNthCalledWith(1, mock.element);
-      expect(this.context.val).toHaveBeenCalledTimes(1);
-      expect(this.context.val).toHaveBeenNthCalledWith(1, 'value');
-      expect(this.context.trigger).toHaveBeenCalledTimes(3);
-      expect(this.context.trigger).toHaveBeenNthCalledWith(1, 'input');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(2, 'change');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(3, 'focusout');
+      expect(mocks.element.value).toEqual('value');
+      expect(mocks.element.dispatchEvent).toHaveBeenCalledTimes(8);
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(1, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(2, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(3, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(4, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(5, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(6, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(7, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(8, expect.any(global.Event));
+      expect(global.Event).toHaveBeenCalledTimes(8);
+      expect(global.Event).toHaveBeenNthCalledWith(1, 'focus', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(2, 'reset', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(3, 'keydown', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(4, 'keypress', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(5, 'keyup', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(6, 'input', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(7, 'change', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(8, 'blur', evtInit);
     });
 
     it('should use default value and return truthy', async () => {
-      this.action = '';
-      this.context.length = 1;
-      mock.element.type = 'text';
-      const actual = await Type.ifExists.call(mock, 'selector');
+      const mocks = createMocks();
+      const actual = await Type.ifExists.call(mocks, 'selector');
       expect(actual).toBeTruthy();
-      expect(mock.$eval).toHaveBeenCalledTimes(2);
-      expect(mock.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), '');
-      expect(mock.$eval).toHaveBeenNthCalledWith(2, 'selector', expect.any(Function));
-      expect(mock.debug).toHaveBeenCalledTimes(1);
-      expect(mock.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s', '', 'selector', 'type');
-      expect(angular.element).toHaveBeenCalledTimes(1);
-      expect(angular.element).toHaveBeenNthCalledWith(1, mock.element);
-      expect(this.context.val).toHaveBeenCalledTimes(1);
-      expect(this.context.val).toHaveBeenNthCalledWith(1, '');
-      expect(this.context.trigger).toHaveBeenCalledTimes(3);
-      expect(this.context.trigger).toHaveBeenNthCalledWith(1, 'input');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(2, 'change');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(3, 'focusout');
-    });
-
-    it('should return falsy', async () => {
-      this.action = '';
-      this.context.length = 0;
-      mock.element.type = 'text';
-      const actual = await Type.ifExists.call(mock, 'selector', 'value', 'label');
-      expect(actual).toBeFalsy();
-      expect(mock.$eval).toHaveBeenCalledTimes(1);
-      expect(mock.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
-      expect(mock.debug).toHaveBeenCalledTimes(1);
-      expect(mock.debug).toHaveBeenNthCalledWith(1, '%s for %s not found', 'selector', 'label');
-      expect(angular.element).toHaveBeenCalledTimes(1);
-      expect(angular.element).toHaveBeenNthCalledWith(1, mock.element);
-      expect(this.context.val).toHaveBeenCalledTimes(1);
-      expect(this.context.val).toHaveBeenNthCalledWith(1, 'value');
-      expect(this.context.trigger).toHaveBeenCalledTimes(3);
-      expect(this.context.trigger).toHaveBeenNthCalledWith(1, 'input');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(2, 'change');
-      expect(this.context.trigger).toHaveBeenNthCalledWith(3, 'focusout');
+      expect(mocks.$eval).toHaveBeenCalledTimes(1);
+      expect(mocks.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), '');
+      expect(mocks.debug).toHaveBeenCalledTimes(1);
+      expect(mocks.debug).toHaveBeenNthCalledWith(1, '%s on %s for %s', '', 'selector', 'type');
+      expect(mocks.element.value).toEqual('');
+      expect(mocks.element.dispatchEvent).toHaveBeenCalledTimes(8);
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(1, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(2, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(3, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(4, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(5, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(6, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(7, expect.any(global.Event));
+      expect(mocks.element.dispatchEvent).toHaveBeenNthCalledWith(8, expect.any(global.Event));
+      expect(global.Event).toHaveBeenCalledTimes(8);
+      expect(global.Event).toHaveBeenNthCalledWith(1, 'focus', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(2, 'reset', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(3, 'keydown', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(4, 'keypress', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(5, 'keyup', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(6, 'input', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(7, 'change', evtInit);
+      expect(global.Event).toHaveBeenNthCalledWith(8, 'blur', evtInit);
     });
 
     it('should log error', async () => {
-      this.action = 'error';
-      this.context.length = 1;
-      mock.element.type = 'text';
-      const actual = await Type.ifExists.call(mock, 'selector', 'value', 'label');
+      const mocks = createMocks({ evalAction: 'error' });
+      const actual = await Type.ifExists.call(mocks, 'selector', 'value', 'label');
       expect(actual).toBeFalsy();
-      expect(mock.$eval).toHaveBeenCalledTimes(1);
-      expect(mock.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
-      expect(mock.debug).toHaveBeenCalledTimes(2);
-      expect(mock.debug).toHaveBeenNthCalledWith(1, 'el.type %s for %s error: %s',
+      expect(mocks.$eval).toHaveBeenCalledTimes(1);
+      expect(mocks.$eval).toHaveBeenNthCalledWith(1, 'selector', expect.any(Function), 'value');
+      expect(mocks.debug).toHaveBeenCalledTimes(2);
+      expect(mocks.debug).toHaveBeenNthCalledWith(1, 'el.type %s for %s error: %s',
         'selector', 'label', expect.any(Error));
-      expect(mock.debug).toHaveBeenNthCalledWith(2, '%s for %s not found', 'selector', 'label');
-      expect(angular.element).not.toHaveBeenCalled();
-      expect(this.context.val).not.toHaveBeenCalled();
-      expect(this.context.trigger).not.toHaveBeenCalled();
+      expect(mocks.debug).toHaveBeenNthCalledWith(2, '%s for %s not found', 'selector', 'label');
+      expect(mocks.element.value).toBeUndefined();
+      expect(mocks.element.dispatchEvent).not.toHaveBeenCalled();
+      expect(global.Event).not.toHaveBeenCalled();
     });
   });
 });
